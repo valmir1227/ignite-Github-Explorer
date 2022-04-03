8bdc5fa8e03a0c833ce09306dc5978cd990d0373
Valmir Almeida <68277445+valmir1227@users.noreply.github.com>
2022-04-03 16:12:38 -0300

    css, mensagem para repositorios sem desc, readmeUp

--- README.md
+++ README.md
@@ -8,4 +8,16 @@
 
 Este projeto é uma aplicação ReactJS com typescript, criado GITHUB EXPLORER. Listando repositorios da Rocketseat
   
+
+## 💻 How to use?
+
+1. No seu terminal digite: <code>git clone https://github.com/sararchh/ignite-Github-Explorer.git</code> 
+
+2. Entre no diretório: <code>cd ignite-Github-Explorer</code>
+
+3. Instale as dependências: <code>yarn</code>
+
+4. Inicie a aplicação: <code>yarn dev</code> 
+
+
 <p  align="center">Made with 💜 by <a  href="https://github.com/sararchh"  target="_blank">Sara Rocha </a></p>
--- src/components/RepositoryItem.tsx
+++ src/components/RepositoryItem.tsx
@@ -3,18 +3,22 @@ interface RepositoryItemProps {
     name: string;
     description: string;
     html_url: string;
-  }
+  };
 }
 
 export function RepositoryItem(props: RepositoryItemProps) {
+  //Se o repositorio estiver sem uma descrição, adiciona uma mensagem...
+  if (!props.repository.description) {
+    props.repository.description =
+      "Desculpe, este repositório ainda está sem descrição 😧";
+  }
+
   return (
     <li>
-      <strong>{props.repository?.name}</strong> 
+      <strong>{props.repository?.name}</strong>
       <p>{props.repository.description}</p>
 
-      <a href={props.repository.html_url}>
-        Acessar repositorio
-      </a>
+      <a href={props.repository.html_url}>Acessar</a>
     </li>
   );
 }
